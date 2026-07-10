cf832966e4450af04cae9679e4827f1afb36ddce
Maroti Bhaskar Shinde <shindemaroti380@gmail.com>
2026-07-10 14:12:32 +0530

    Create swap_two_numbers.java

---
+++ swap_two_numbers.java
@@ -0,0 +1,12 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
